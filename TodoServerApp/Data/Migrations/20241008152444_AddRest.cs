2bf706b22a3355cad0272027749c0142d6e06abe
GoginMisha <partycat53241@gmail.com>
2024-10-08 21:10:51 +0500

    Lesson6

---
+++ TodoServerApp/Data/Migrations/20241008152444_AddRest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoServerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 8, 20, 24, 43, 968, DateTimeKind.Local).AddTicks(6121));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 8, 20, 24, 43, 968, DateTimeKind.Local).AddTicks(6139));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 8, 20, 24, 43, 968, DateTimeKind.Local).AddTicks(6140));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 8, 20, 24, 43, 968, DateTimeKind.Local).AddTicks(6141));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 7, 17, 10, 16, 559, DateTimeKind.Local).AddTicks(1379));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 7, 17, 10, 16, 559, DateTimeKind.Local).AddTicks(1391));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 7, 17, 10, 16, 559, DateTimeKind.Local).AddTicks(1393));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 10, 7, 17, 10, 16, 559, DateTimeKind.Local).AddTicks(1394));
+        }
+    }
+}
--- TodoServerApp/Data/Services/MSSQLDataService.cs
+++ TodoServerApp/Data/Services/MSSQLDataService.cs
@@ -5,9 +5,33 @@ namespace TodoServerApp.Data.Services
 {
 	public class MSSQLDataService(ApplicationDbContext context) : IDataService
 	{
-		public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
+		public async Task<IEnumerable<TaskItem>> GetAllAsync()
 		{
 			return await context.TaskItems.ToArrayAsync();
+		}
+
+		public async Task SaveAsync(TaskItem taskitem)
+		{
+			if (taskitem.Id == 0)
+			{
+				taskitem.CreatedDate = DateTime.Now;
+				await context.TaskItems.AddAsync(taskitem);
+			}
+            else
+            {
+                context.TaskItems.Update(taskitem);
+            }
+			await context.SaveChangesAsync();
+        }
+		public async Task<TaskItem> GetTaskAsync(int id)
+		{
+			return await context.TaskItems.FirstAsync(x => x.Id == id);
+		}
+		public async Task DeleteAsync(int id)
+		{
+			var taskItem = await context.TaskItems.FirstAsync(x => x.Id == id);
+			context.TaskItems.Remove(taskItem);
+			await context.SaveChangesAsync();
 		}
 	}
 }
